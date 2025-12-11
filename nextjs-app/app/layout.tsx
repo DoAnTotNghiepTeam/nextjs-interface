d0a79702a4f9d8c165a59f17cc1e38bdabe80ea0
Lê Nguyễn Quốc Huy <huynq2.21it@vku.udn.vn>
2025-12-11 15:22:35 +0700

    update code

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
 };
 
-//  moqia Cap nhat
+//  test quá trình cập nhật 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
